cafd77287f7c20975ff31437e60ae6688acbca51
diebomb <diego.bombin@estudiantes.uva.es>
2024-11-21 21:21:16 +0100

    docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-spring-docker-project/',
   server: {
     proxy: {
       '/api': 'http://localhost:8080'
